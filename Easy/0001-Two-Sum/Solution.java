a3ca564020161e44649db29607e2628d5d684c47
cser-sanidhya <cser.sanidhya@gmail.com>
2026-07-15 20:37:48 +0530

    Update Solution.java

--- Easy/0001-Two-Sum/Solution.java
+++ Easy/0001-Two-Sum/Solution.java
@@ -1,13 +1,4 @@
-# 1. Two Sum
-## Problem Statement
-Find two numbers whose sum equals the target.
-## My Approach
-  1.Traverse the array once.
-  2.Calculate the required complement.
-  3.Check if the complement exists in a HashMap.
-  4.If found, return the indices.
-  5.Otherwise, store the current number and its index.
-  ## Java Solution
+
   class Solution {
     public int[] twoSum(int[] nums, int target) {
         for(int i=0;i<nums.length;i++)
@@ -24,8 +15,4 @@ Find two numbers whose sum equals the target.
     }
 }
 
-## Time Complexity
-  O(n*n)
-## Space Complexity
-  O(1)
   
