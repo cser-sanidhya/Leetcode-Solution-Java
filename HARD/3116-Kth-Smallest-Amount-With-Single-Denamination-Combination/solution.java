b52844d9e726be435e61ff62e9801ae328f2ed9d
cser-sanidhya <cser.sanidhya@gmail.com>
2026-08-21 18:10:25 +0530

    Create solution.java

---
+++ HARD/3116-Kth-Smallest-Amount-With-Single-Denamination-Combination/solution.java
@@ -0,0 +1,77 @@
+class Solution {
+    public long findKthSmallest(int[] coins, int k) {
+        long left = 1;
+        long right = (long) coins[0] * k;
+
+        for (int coin : coins) {
+            right = Math.min(right, (long) coin * k);
+        }
+
+        while (left < right) {
+            long mid = left + (right - left) / 2;
+
+            if (count(mid, coins) >= k) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+
+    private long count(long x, int[] coins) {
+        int n = coins.length;
+        long total = 0;
+
+        for (int mask = 1; mask < (1 << n); mask++) {
+            long lcm = 1;
+            int bits = 0;
+            boolean valid = true;
+
+            for (int i = 0; i < n; i++) {
+                if ((mask & (1 << i)) != 0) {
+                    bits++;
+
+                    lcm = lcm(lcm, coins[i]);
+
+                    if (lcm > x) {
+                        valid = false;
+                        break;
+                    }
+                }
+            }
+
+            if (!valid) {
+                continue;
+            }
+
+            long contribution = x / lcm;
+
+            if ((bits & 1) == 1) {
+                total += contribution;
+            } else {
+                total -= contribution;
+            }
+        }
+
+        return total;
+    }
+
+    private long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
+    }
+
+    private long lcm(long a, long b) {
+        long g = gcd(a, b);
+        return (a / g) * b;
+
+        
+    }
+}
+
