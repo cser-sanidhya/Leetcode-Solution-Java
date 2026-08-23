c72fc31ab3ffb713ed8e5c85ec9ee008618883ec
cser-sanidhya <cser.sanidhya@gmail.com>
2026-08-23 18:50:20 +0530

    Create solution.java

---
+++ Medium/1927-Sum-Game/solution.java
@@ -0,0 +1,35 @@
+class Solution {
+    public boolean sumGame(String num) {
+        int n = num.length();
+        int mid = n / 2;
+
+        int leftSum = 0;
+        int rightSum = 0;
+        int leftQ = 0;
+        int rightQ = 0;
+
+        for (int i = 0; i < mid; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                leftQ++;
+            } else {
+                leftSum += c - '0';
+            }
+        }
+
+        for (int i = mid; i < n; i++) {
+            char c = num.charAt(i);
+
+            if (c == '?') {
+                rightQ++;
+            } else {
+                rightSum += c - '0';
+            }
+        }
+
+        return 2 * (leftSum - rightSum)
+                != 9 * (rightQ - leftQ);
+        
+    }
+}
