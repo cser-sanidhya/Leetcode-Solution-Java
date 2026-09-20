f01ae94777ac443da69cd5dc65e59bd0bce2bc8d
cser-sanidhya <cser.sanidhya@gmail.com>
2026-09-20 19:33:04 +0530

    Create solution.java

---
+++ Medium/102-Binary-Tree-Level-Order-Traversal/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (root == null) return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            List<Integer> currentLevel = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode node = queue.poll();
+
+                currentLevel.add(node.val);
+
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+
+            result.add(currentLevel);
+        }
+
+        return result;
+    }
+}
